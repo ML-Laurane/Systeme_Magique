2e85086c48de21a846baa6e17ec7b2d62f31196e
mouronval.laurane <laurane.mouronval@univ-littoral.fr>
2021-12-03 00:34:48 +0100

    test redirection 2

--- src/index.php
+++ src/index.php
@@ -2,6 +2,8 @@
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 echo '<p>update : 02 dec. 2021 </p>';
-phpinfo();
+
+#header('Location: /sauvetage.php'); 
+require('./src/sauvetage.php');
 ?>
 
