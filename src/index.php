8246d42d8853c8571cf19550a2daa1b5d0aac108
ML-Laurane <94927286+ML-Laurane@users.noreply.github.com>
2021-12-03 05:26:27 +0100

    Delete index.php

--- src/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 02 dec. 2021 </p>';
-
-#header('Location: /sauvetage.php'); 
-require('./src/sauvetage.php');
-?>
-
